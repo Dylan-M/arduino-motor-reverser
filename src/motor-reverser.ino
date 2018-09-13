d4e4add7b47c080a27a763b3b3addebd4c094ab0
Dylan Myers <dylan.myers@bluemedora.com>
2018-09-13 10:49:25 -0400

    Initial source code submission

---
+++ src/motor-reverser.ino
@@ -0,0 +1,83 @@
+/*
+ * Auto-reverse circuit for DC motors with dual control switches
+ * With code based on Adafruit Arduino - Lesson 15. Bi-directional Motor
+ */
+
+#define DEBUG 1
+
+int enablePin = 11;
+int in1Pin = 10;
+int in2Pin = 9;
+int switchPinA = 7;
+int switchPinB = 6;
+boolean reverse;
+boolean enableSwitchPinA;
+boolean enableSwitchPinB;
+
+void setup()
+{
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  pinMode(switchPinA, INPUT_PULLUP);
+  pinMode(switchPinB, INPUT_PULLUP);
+  reverse = false;
+  enableSwitchPinA = true;
+  enableSwitchPinB = false;
+}
+
+void loop()
+{
+  if (enableSwitchPinA) {
+    checkSwitchPinA();
+  }
+  if (enableSwitchPinB) {
+    checkSwitchPinB();
+  }
+  #ifdef DEBUG
+    Serial.print(F("Reverse: "));
+    Serial.println(reverse);
+    Serial.print(F("Pin A: "));
+    Serial.println(enableSwitchPinA);
+    Serial.print(F("Pin B: "));
+    Serial.println(enableSwitchPinB);
+    Serial.flush();
+  #endif
+  setMotor(reverse);
+}
+
+void setMotor(boolean reverse)
+{
+  analogWrite(enablePin, 255);
+  digitalWrite(in1Pin, ! reverse);
+  digitalWrite(in2Pin, reverse);
+}
+
+void checkSwitchPinA() {
+  if (digitalRead(switchPinA) == HIGH && digitalRead(switchPinB) == LOW) {
+    #ifdef DEBUG
+      Serial.println(F("Executing Switch Pin A"));
+      Serial.flush();
+    #endif
+    enableSwitchPinA = false;
+    enableSwitchPinB = true;
+    delay(random(5,12) * 1000);
+    reverse = ! reverse;
+  }
+}
+
+void checkSwitchPinB() {
+  if (digitalRead(switchPinB) == HIGH && digitalRead(switchPinA) == LOW) {
+    #ifdef DEBUG
+      Serial.println(F("Executing Switch Pin A"));
+      Serial.flush();
+    #endif
+    enableSwitchPinA = true;
+    enableSwitchPinB = false;
+    delay(random(5,12) * 1000);
+    reverse = ! reverse;
+  }
+}
